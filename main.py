821539c50132d23e5a6755cb1205da839fc8eeef
Felex Kuria <engineerfelex@gmail.com>
2024-06-08 23:56:31 +0300

    testing  variable knowledege

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-felex=input("What is Your Name?")
-print("Hello "+" " +felex)
+# felex=input("What is Your Name?")
+# print("Hello "+" " +felex)
+
+a=input("a:")
+b=input("b:")
+
+f=a
+d=b
+
+b=f
+a=d
+
+ 
+print("a="+a)
+print("b="+b)
+
