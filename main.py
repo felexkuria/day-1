16c87b1a5ec7a744b907a89c3f06d7d9da6af2c7
Felex Kuria <engineerfelex@gmail.com>
2024-06-09 00:08:12 +0300

    project name generator

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 # felex=input("What is Your Name?")
 # print("Hello "+" " +felex)
 
-a=input("a:")
-b=input("b:")
+# a=input("a:")
+# b=input("b:")
 
-f=a
-d=b
+# f=a
+# d=b
 
-b=f
-a=d
+# b=f
+# a=d
 
  
-print("a="+a)
-print("b="+b)
+# print("a="+a)
+# print("b="+b)
 
+print("Welcome to Band Name Generator.")
+city= input("What is name of the city you grew up in?\n")
+pet_name=input("what is your pet's name?")
+
+print("Your band name could be" +" "+city+" "+pet_name)
